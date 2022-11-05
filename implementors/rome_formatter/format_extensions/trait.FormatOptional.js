dca5276e2f02bf3ff1add91460a8585de66a23dc
nissy-dev <nissy-dev@users.noreply.github.com>
2022-11-05 08:10:39 +0000

    deploy: 20c46ac2a29ff4ba6212eebf4ffe0b9411acca49

--- implementors/rome_formatter/format_extensions/trait.FormatOptional.js
+++ implementors/rome_formatter/format_extensions/trait.FormatOptional.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["rome_js_formatter"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"rome_js_formatter":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/rome_rowan/ast/mutation/trait.AstNodeExt.js
+++ implementors/rome_rowan/ast/mutation/trait.AstNodeExt.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["rome_rowan"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"rome_rowan":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
